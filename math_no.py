45a051f0a2d55ffb3821a164b1c0c688f5120f75
Hagb Green <hagb_green@qq.com>
2020-12-06 14:33:30 +0800

    支持多个单行公式连写

--- math_no.py
+++ math_no.py
@@ -1,4 +1,5 @@
 import panflute as pf
+import copy
 
 valign_block = pf.RawBlock('<w:tcPr><w:vAlign w:val="center"/></w:tcPr>',
                            format="openxml")
@@ -7,29 +8,43 @@ valign_block = pf.RawBlock('<w:tcPr><w:vAlign w:val="center"/></w:tcPr>',
 class MathReplace():
     def action(self, elem, doc):
         pf.debug('s:', elem)
-        if isinstance(elem, pf.Para) and len(elem.content) == 1:
-            elem1 = elem.content[0]
-            if isinstance(elem1, pf.Math) and elem1.format == 'DisplayMath':
-                elem = pf.Table(
-                    pf.TableBody(
-                        pf.TableRow(
-                            pf.TableCell(valign_block, pf.Plain(pf.Str('a'))),
-                            pf.TableCell(valign_block, pf.Plain(elem1)),
-                            pf.TableCell(valign_block,
-                                         pf.Plain(pf.Str('a'))))),
-                    colspec=[('AlignLeft', 0.1), ('AlignCenter', 0.8),
-                             ('AlignRight', 0.1)],
-                    caption=pf.Caption()
-                    #attributes={
-                    #   "alignment":
-                    #   ['AlignLeft', 'AlignCenter', 'AlignRight'],
-                    #   "width": [0.1, 0.8, 0.1]
-                    #                }
-                )
-                pf.debug(elem.caption)
-
-        if isinstance(elem, pf.Table):
-            pf.debug(elem.caption)
+        if isinstance(elem, pf.Para):
+            rows = []
+            content_group = []
+            for elem1 in elem.content:
+                if isinstance(elem1, (pf.Space, pf.SoftBreak)):
+                    if content_group and content_group[-1][0]:
+                        continue
+                is_math = isinstance(elem1,
+                                     pf.Math) and elem1.format == 'DisplayMath'
+                if content_group:
+                    if content_group[-1][0] == is_math:
+                        content_group[-1][1].append(elem1)
+                        continue
+                content_group.append([is_math, [elem1]])
+            elem_old = elem
+            elem = []
+            for elem_group in content_group:
+                if elem_group[0]:
+                    rows = []
+                    for math_elem in elem_group[1]:
+                        rows.append(
+                            pf.TableRow(
+                                pf.TableCell(),
+                                pf.TableCell(valign_block,
+                                             pf.Plain(math_elem)),
+                                pf.TableCell(valign_block,
+                                             pf.Plain(pf.Str('a')))))
+                    elem.append(
+                        pf.Table(pf.TableBody(*rows),
+                                 colspec=[('AlignLeft', 0.1),
+                                          ('AlignCenter', 0.8),
+                                          ('AlignRight', 0.1)],
+                                 caption=pf.Caption(pf.Div())))
+                else:
+                    elem_new = copy.copy(elem_old)
+                    elem_new.content = elem_group[1]
+                    elem.append(elem_new)
         pf.debug('o:', elem)
         return elem
 
