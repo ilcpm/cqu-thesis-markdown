2efb8885a48fca02f26ad91b8de79efa0da6bbb5
Hagb Green <hagb_green@qq.com>
2020-12-09 00:40:06 +0800

    Add support for reference

---
+++ refs.py
@@ -0,0 +1,33 @@
+import panflute as pf
+
+
+class refsReplacer():
+    def find(self, elem, doc=None):
+        if hasattr(elem, 'identifier') and elem.identifier:
+            self.bookmarks.add(elem.identifier)
+            pf.debug('anchor:', elem.identifier)
+        return elem
+
+    def replace(self, elem, doc):
+        if isinstance(elem, pf.Cite):
+            elem: pf.Cite
+            citation: pf.Citation = elem.citations[0]
+            pf.debug(citation.id)
+            if citation.id in self.bookmarks:
+                elem = pf.RawInline(
+                    f'<w:fldSimple w:instr=" REF {citation.id} eq1 \\h "/>',
+                    format="openxml")
+        return elem
+
+    def __init__(self):
+        self.bookmarks = set()
+
+
+def main(doc=None):
+
+    refs_replacer = refsReplacer()
+    return pf.run_filters([refs_replacer.find, refs_replacer.replace], doc=doc)
+
+
+if __name__ == "__main__":
+    main()
