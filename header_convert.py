8baf017ec506968421848cd4bdc5a5077e267119
ilcpm <ilcpm@outlook.com>
2020-12-25 12:40:25 +0800

    更改了标题不编号的处理逻辑，直接插入ooxml取消编号，不引入第三方样式

--- header_convert.py
+++ header_convert.py
@@ -1,16 +1,22 @@
-#!/usr/bin/env python3
 # 处理标题样式的编号
 
 import panflute as pf
 
+sec_no_num = '''
+<w:pPr>
+    <w:numPr>
+        <w:numId w:val="0"/>
+    </w:numPr>
+</w:pPr>
+'''
 
-def apply_style_header(elem: pf.Header, style: str):
-    style_raw = style.replace(' ', '')
-    elem.content.insert(
-        0,
-        pf.RawInline(f'<w:pPr><w:pStyle w:val="{style_raw}"/></w:pPr>',
-                     format="openxml"))
-    return elem
+
+# def apply_style_header(elem: pf.Header, style: str):
+#     style_raw = style.replace(' ', '')
+#     elem.content.insert(0,
+#                         pf.RawInline(f'<w:pPr><w:pStyle w:val="{style_raw}"/></w:pPr>',
+#                                      format="openxml"))
+#     return elem
 
 
 appendix = False
@@ -31,8 +37,10 @@ def headerConvert(elem, doc):
         # 暂时用不到
         # elif appendix:
         #     apply_style_header(elem, f"Appendix Heading {elem.level}")
-        if 'unnumbered' in elem.classes:  # and (elem.level == 1 or not appendix):
-            apply_style_header(elem, f"Unnumbered Heading {elem.level}")
+        # and (elem.level == 1 or not appendix):
+        if 'unnumbered' in elem.classes:
+            # apply_style_header(elem, f"Unnumbered Heading {elem.level}")
+            elem.content.insert(0, pf.RawInline(sec_no_num, format='openxml'))
         if elem.identifier:
             elem.content = [pf.Span(*elem.content, identifier=elem.identifier)]
             elem.identifier = ""
--- template/template.md
+++ template/template.md
@@ -33,6 +33,7 @@ And so even though we face the difficulties of today and tomorrow, I still have 
 I have a dream that one day this nation will rise up and live out the true meaning of its creed: "We hold these truths to be self-evident, that all men are created equal."
 
 I have a dream that one day on the red hills of Georgia, the sons of former slaves and the sons of former slave owners will be able to sit down together at the table of brotherhood.
+
 I have a dream that one day even the state of Mississippi, a state sweltering with the heat of injustice, sweltering with the heat of oppression, will be transformed into an oasis of freedom and justice.
 
 I have a dream that my four little children will one day live in a nation where they will not be judged by the color of their skin but by the content of their character.
@@ -90,7 +91,10 @@ I have a dream today!
 ## 标题的交叉引用{#jcyy}
 
 测试标题的交叉引用：[@sec-jcyy-no]
+
 页码：[@page-jcyy]
+
+标题文本：[@sec-jcyy]
 
 # 其他自定义功能
 
