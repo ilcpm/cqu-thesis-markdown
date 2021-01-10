d14d89bbd1c7e58e5c7714192c1d6cb3dd0ee9e6
ilcpm <ilcpm@outlook.com>
2021-01-11 01:08:43 +0800

    修bug，页面上边距设置错误

--- texcommands.py
+++ texcommands.py
@@ -3,7 +3,10 @@ import typing
 import re
 index_str = "目录"
 
-pageInfoStr = r'''<w:pgSz w:w="11907" w:h="16840" w:code="9"/><w:pgMar w:top="1134" w:right="1418" w:bottom="1418" w:left="1418" w:header="907" w:footer="851" w:gutter="567"/>'''
+pageInfoStr = r'''
+<w:pgSz w:w="11907" w:h="16840" w:code="9"/>
+<w:pgMar w:top="1701" w:right="1418" w:bottom="1418" w:left="1418" w:header="907" w:footer="851" w:gutter="567"/>
+'''
 header_footer = r"""
 <w:headerReference w:type="even" r:id="rId9" />
 <w:headerReference w:type="default" r:id="rId11" />
