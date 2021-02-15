bd6da7ded5c365581decd165e64e1e9273dec182
Hagb Green <hagb_green@qq.com>
2021-02-16 07:06:05 +0800

    Move filters to https://github.com/ilcpm/pandoc_markdown2docx_instuction

--- template/make.ps1
+++ template/make.ps1
@@ -1,11 +1,7 @@
 pandoc.exe `
     .\template.md `
     -o .\template.docx `
-    --filter ..\header_convert.py `
-    --filter ..\equations_no.py `
-    --filter ..\figures_no.py `
-    --filter ..\refs.py `
-    --filter ..\texcommands.py `
+    --filter docx_filters `
     --citeproc `
     --reference-doc .\reference.docx
     # --filter ..\section_break.py `
