e82378f1922ef57bd9ee291beaa289315998f25a
ilcpm <ilcpm@outlook.com>
2021-05-11 20:00:48 +0800

    修改文档可读性，增加了大量注释

--- template/make.ps1
+++ template/make.ps1
@@ -1,9 +1,11 @@
-$target = "template"
+# 转换脚本
+$target = "template" # 转换的文件文件名
 $in = $target + ".md"
 $out = $target + ".docx"
-$isOpenDocxAfterComplete = 1
+$isOpenDocxAfterComplete = 1 # 转换完成是否需要自动打开
 
-
+# 可以在下面按照顺序添加多个文件，会按照顺序合并在一起，实现多文档组织结构
+# 这里默认是由“head.md”和上面的$target文件合并得到的
 pandoc.exe `
     head.md `
     $in `
